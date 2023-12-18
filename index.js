7ceb91717d3c1824b66edfa95d24caee3e7c0791
Matt McCormack <matthewmccormack@gmail.com>
2023-12-18 15:03:21 +0000

    Fix the tests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     },
     'import/internal-regex': '^src/'
   },
@@ -10,57 +10,57 @@ module.exports = {
     es2021: true
   },
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "react-app",
-    "react-app/jest",
-    "plugin:react/recommended",
-    "standard"
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'react-app/jest',
+    'plugin:react/recommended',
+    'standard'
   ],
   plugins: [
     '@typescript-eslint',
     'no-relative-import-paths'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "no-trailing-spaces": "error",
-    "max-len": ["error", { "code": 120 }],
-    "func-style": ["error", "expression"],
-    "import/no-relative-parent-imports": "error",
-    "no-relative-import-paths/no-relative-import-paths": [
-      "warn",
-      { "allowSameFolder": true }
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-trailing-spaces': 'error',
+    'max-len': ['error', { code: 120 }],
+    'func-style': ['error', 'expression'],
+    'import/no-relative-parent-imports': 'error',
+    'no-relative-import-paths/no-relative-import-paths': [
+      'warn',
+      { allowSameFolder: true }
     ],
-    "react/hook-use-state": [2],
-    "import/order": [
-      "error",
+    'react/hook-use-state': [2],
+    'import/order': [
+      'error',
       {
-        groups: ["builtin", "external", "internal"],
+        groups: ['builtin', 'external', 'internal'],
         pathGroups: [
           {
-            pattern: "react",
-            group: "external",
-            position: "before"
+            pattern: 'react',
+            group: 'external',
+            position: 'before'
           }
         ],
-        pathGroupsExcludedImportTypes: ["react"],
-        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true
         }
       }
     ],
     indent: [
-      "error",
+      'error',
       2,
       {
         SwitchCase: 1,
@@ -68,5 +68,5 @@ module.exports = {
       }
     ]
   },
-  ignorePatterns: ["node_modules/*", "build/*"]
-};
+  ignorePatterns: ['node_modules/*', 'build/*']
+}
